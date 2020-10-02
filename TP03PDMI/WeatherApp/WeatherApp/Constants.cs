da9c53a16eac3e0943ce4af86128f6dc008f4d18
Danilo <dac.08@hotmail.com>
2020-10-01 23:28:03 -0300

    Exercicio 06 PDMI

---
+++ TP03PDMI/WeatherApp/WeatherApp/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeatherApp
+{
+    public static class Constants
+    {
+        public const string URL = "https://api.openweathermap.org/data/2.5/weather";
+        public const string KEY = "0ae116e1df854c2f4a900334b3a163e6";
+    }
+}
